518fb3f8ca25ff92c43fc719e15d980308f2efeb
Sebastian Stöckl <sebastian.stoeckl@uni.li>
2025-08-10 16:56:33 +0200

    lets see whether plotting works now

--- docs/r/ucase1.R
+++ docs/r/ucase1.R
@@ -2,14 +2,14 @@
 ucase1 <- function(rate, nper, pv) {
   stopifnot(is.numeric(rate), is.numeric(nper), is.numeric(pv))
   t <- 0:nper
-  fv_path <- -pv * (1 + rate)^t
+  fv_path <- pv * (1 + rate)^t      # positive FV path
   list(
     ok = TRUE,
     inputs  = list(rate = rate, nper = nper, pv = pv),
     results = list(
-      fv = tail(fv_path, 1),
-      t  = t,
-      fv_path = fv_path
+      fv = tail(fv_path, 1),        # final FV (positive)
+      t  = t,                       # 0..nper
+      fv_path = fv_path             # full path for plotting
     )
   )
 }
