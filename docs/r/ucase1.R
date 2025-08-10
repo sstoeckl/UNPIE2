561c1b8ff2366ab82fad6c01cb18fd76f732fb51
Sebastian Stöckl <sebastian.stoeckl@uni.li>
2025-08-10 16:53:15 +0200

    Update for plotting

--- docs/r/ucase1.R
+++ docs/r/ucase1.R
@@ -1,11 +1,15 @@
 # docs/r/ucase1.R
 ucase1 <- function(rate, nper, pv) {
   stopifnot(is.numeric(rate), is.numeric(nper), is.numeric(pv))
-  # Sign convention: in your old UI pv was passed as -pv; here we'll keep it explicit.
-  fv <- (-pv) * (1 + rate)^nper
+  t <- 0:nper
+  fv_path <- -pv * (1 + rate)^t
   list(
     ok = TRUE,
     inputs  = list(rate = rate, nper = nper, pv = pv),
-    results = list(fv = fv)
+    results = list(
+      fv = tail(fv_path, 1),
+      t  = t,
+      fv_path = fv_path
+    )
   )
 }
